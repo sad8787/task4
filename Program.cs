ae0f88630df51662f953c59b7d3ce195b6bcb3bd
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-07-30 13:33:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = "";
+            if (args.Length != 1)
+                Console.WriteLine("Error");
+            else
+            {
+                text1 = args[0];
+                
+            }
+            TextReader leer1 = new StreamReader(text1);
+            List<double> vs = new List<double>();
+            string l;
+            while ((l = leer1.ReadLine()) != null)
+            {
+                vs.Add( double.Parse(l)); 
+            }            
+            int averge = Convert.ToInt32(vs.Average());
+            int Steps = 0;
+            foreach (var item in vs)
+            {
+                int a = Convert.ToInt32(item);
+                if (a < averge)
+                {
+                    Steps += averge - a;
+                }
+                else
+                {
+                    Steps += a - averge;
+                }
+            }
+            Console.WriteLine(Steps);
+            Console.ReadKey();
+        }
+    }
+}
